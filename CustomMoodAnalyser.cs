88dbd780862ff84236c8fe52b383181989293125
kanakbabar <kanakbabar999@gamil.com>
2021-10-13 21:59:57 +0530

    [kanak]Add : Day20_21 Assignment problem

---
+++ CustomMoodAnalyser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20_Assignment
+{
+   
+    
+        public class CustomMoodAnalyser : Exception
+        {
+            public enum ExceptionType
+            {
+                Entered_null,
+                Entered_Empty_String,
+                No_Such_Class,
+                No_Such_Method,
+                No_Such_Field
+            }
+
+            ExceptionType enumtype;
+
+            public CustomMoodAnalyser(ExceptionType type, string msg) : base(msg)
+            {
+                this.enumtype = type;
+            }
+        }
+    
+}
---
+++ MoodAnalyser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20_Assignment
+{
+   public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser()
+        {
+
+        }
+        public MoodAnalyser(string msg)
+        {
+            this.message = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new CustomMoodAnalyser(CustomMoodAnalyser.ExceptionType.Entered_Empty_String, "String is Empty");
+                }
+
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomMoodAnalyser(CustomMoodAnalyser.ExceptionType.Entered_null, "Nothing is entered");
+            }
+        }
+    }
+}
